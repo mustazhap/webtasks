1b16f4ad570e0611b473e9254a795bf88eee3f77
Rustem Mustazhap <r10181998@gmail.com>
2017-10-08 08:00:22 +0600

    changes of week2 and week 5, task 1 completed

---
+++ week5/script1.js
@@ -0,0 +1,21 @@
+
+function myFunction() {
+  var ids = ["s11","s12","s13","s21","s22","s23","s31","s32","s33"];
+  var x = [];
+  for (var i = 0; i < ids.length; i++) {
+      x[i] = document.getElementById(ids[i]).value;
+      
+  }
+  console.log(x);
+      
+  var result = x[0]*(x[4]*x[8]-x[5]*x[7])-x[1]*(x[3]*x[8]-x[5]*x[6])+x[2]*(x[3]*x[7]-x[4]*x[6]);
+
+  
+ 
+  document.getElementById("result").innerHTML = result;
+
+}
+
+
+
+
